6ca21c514fe67d17986405da02aa5e09322fe08e
bonecrushereb <bonecrushereb@gmail.com>
2016-04-18 09:24:01 -0700

    router_test.js is passing

--- test/router_test.js
+++ test/router_test.js
@@ -1,13 +1,29 @@
-const expect = require('chai').expect;
-const fs = require('fs');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request;
 const Router = require(__dirname + '/../lib/router');
+
+const router = new Router();
 
 describe('rainier-beer router', () => {
   it('should be a router', () => {
-    expect(Router.routes).to.have.property('GET');
-    expect(Router.routes).to.have.property('POST');
-    expect(Router.routes).to.have.property('PUT');
-    expect(Router.routes).to.have.property('PATCH');
-    expect(Router.routes).to.have.property('DELETE');
+    expect(router.routes).to.have.property('GET');
+    expect(router.routes).to.have.property('POST');
+    expect(router.routes).to.have.property('PUT');
+    expect(router.routes).to.have.property('PATCH');
+    expect(router.routes).to.have.property('DELETE');
+  });
+
+  it('should respond with an error with an invalid route', (done) => {
+    request('localhost:3000')
+    .get('/badroute')
+    .end((err, res) => {
+      expect(err).to.not.eql(null);
+      expect(res).to.have.status(404);
+      expect(res.text).to.eql('404 Not Found');
+      done();
+    });
   });
 });
